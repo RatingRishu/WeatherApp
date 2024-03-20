22d9567b0e8c2956380119bb228ab9b3e1a90b27
RatingRishu <rairishabh5001@gmail.com>
2024-03-20 15:55:28 +0530

    first commit

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WeathersService } from '../weathers.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-weather',
@@ -8,19 +9,28 @@ import { WeathersService } from '../weathers.service';
 })
 export class WeatherComponent implements OnInit {
   myweather: any;
+  cityname: any;
   temperature: number = 0;
   mintemp: number = 0;
   maxtemp: number = 0;
   humidity: number = 0;
   wind: number = 0;
-  city: string = 'London';
+  city: string = 'Buxar';
   sunset: number = 0;
   sunrise: number = 0;
+  description : any; 
   currenttime: number = 0;
-
-
+  day2: number = 0;
+  day3: number = 0;
+  day4: number = 0;
+  day5: number = 0;
+  foredate2: any;
+  foredate3: any;
+  foredate4: any;
+  foredate5: any;
+  now = new Date()
   constructor(private weathersService: WeathersService) {
-
+   
   }
   ngOnInit(): void {
 
@@ -42,6 +52,8 @@ export class WeatherComponent implements OnInit {
       this.myweather = res;
       console.log(res)
       console.log(this.myweather);
+      console.log(this.now)
+      this.cityname = this.myweather.name;
       this.temperature = this.myweather.main.temp;
       this.mintemp = this.myweather.main.temp_min;
       this.maxtemp = this.myweather.main.temp_max;
@@ -50,14 +62,37 @@ export class WeatherComponent implements OnInit {
       this.sunset = this.myweather.sys.sunset;
       this.sunrise = this.myweather.sys.sunrise;
       this.currenttime = this.myweather.coord.dt;
-
+      this.description =this.myweather.weather[0].description;
     },
 
     complete: () => console.log('API call Success')
 
   })
   this.weathersService.getweathernextdays(city).subscribe({
-
+    next: (res) => {
+      this.myweather = res;
+      console.log(res)
+      this.day2 = this.myweather.list[1].main.temp
+      this.day3 = this.myweather.list[2].main.temp
+      this.day4 = this.myweather.list[3].main.temp
+      this.day5 = this.myweather.list[4].main.temp
+      this.foredate2 = this.myweather.list[4].dt_txt;
+      this.foredate3 = this.myweather.list[12].dt_txt;
+      this.foredate4 = this.myweather.list[20].dt_txt;
+      this.foredate5 = this.myweather.list[29].dt_txt;
+    },
   })
 }
+getCurrentDateTime(): string {
+  const currentDateTime = new Date();
+  return currentDateTime.toDateString() ;
 }
+}
+// 
+// + ' ' + currentDateTime.toLocaleTimeString()
+//      _                      _                 ____ _     ___ 
+//     / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
+//    / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | | 
+//   / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | | 
+//  /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
+//                 |___/
